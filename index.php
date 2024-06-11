9237245e8ae7c9f5beb25ebcfe42e81bac063e68
Pavan Dusane <167501800+dusanepavan@users.noreply.github.com>
2024-06-11 08:45:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,286 @@
+<!Doctype htlm>
+<html lang="eng">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"><a href="#" class="fas fa-heart"></a>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fruit Plant Website</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- <style> -->
+        <!-- body{ -->
+        <!-- background-image: url('background.jpg'); -->
+        <!-- background-repeat: no-repeat; -->
+        <!-- } -->
+    <!-- </style> -->
+</head>
+<body>
+    <header>
+        <div class="header-1">
+        <a href="#" class="logo"><i class="fa fa-shopping-basket"></i>PLANT</a>
+
+        <form action="" class="search-box-container">
+            <input type="search" id="search-box" placeholder="serach here...">
+            <label for="search-box" class="fa fa-search"></label>
+        </form>
+
+        </div>
+
+        <div class="header-2">
+            <!-- <div id="menu-bar" class="fas"></div> -->
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <!-- <a href="#category">category</a> -->
+            <a href="#product">product</a>
+            <!-- <a href="#deal">deal</a> -->
+            <a href="#contact">contact</a>
+        </nav>
+        
+        <div class="icons">
+            <!-- <i class="fas fa-search"></i> -->
+            <i class="fa fa-heart"></i>
+            <i class="fa fa-shopping-cart"></i>
+            <i class="fa fa-user-circle"></i>
+        </div>
+        </div>
+    </header>
+
+ <section class="home" id="home">
+    <div class="image">
+        <img src="background3.png" alt="">
+    </div>
+
+    <div class="content">
+        <span>fresh</span>
+        <h3>Plant a green tree and live younger</h3>
+        <a href="#" class="btn">Get started</a>
+    </div>
+ </section>   
+ <section class="product" id="product">
+    <h1 class="heading">Latest <span>varaities</span></h1>
+    <div class="box-container">
+        <div class="box">
+            <span class="discount ">50%</span>
+        <div class="icons">
+            <a href="#" class="fa fa-heart"></a>
+            <a href="#" class="fa fa-share"></a>
+            <a href="#" class="fa fa-eye"></a>
+        </div>
+            <img src="p2.jpg" alt="">
+            <h3>grapes plant</h3>
+            <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-alt"></i> 
+            </div>
+            <div class="price">rs.500<span>rs.1000</span></div>
+            <div class="quantity">
+                <span>quantity : </span>
+                <input type="number" min="1" max="1000" value="1">
+            </div>
+            <a href="#" class="btn">add to cart</a>
+        </div>
+        
+        <div class="box">
+            <span class="discount ">50%</span>
+        <div class="icons">
+            <a href="#" class="fa fa-heart"></a>
+            <a href="#" class="fa fa-share"></a>
+            <a href="#" class="fa fa-eye"></a>
+        </div>
+            <img src="p3.jpg" alt="">
+            <h3>Mango plant</h3>
+            <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-alt"></i>
+            </div>
+            <div class="price">rs.200<span>rs.300</span></div>
+            <div class="quantity">
+                <span>quantity : </span>
+                <input type="number" min="1" max="1000" value="1">
+            </div>
+            <a href="#" class="btn">add to cart</a>
+        </div>
+
+        <div class="box">
+            <span class="discount ">50%</span>
+        <div class="icons">
+            <a href="#" class="fa fa-heart"></a>
+            <a href="#" class="fa fa-share"></a>
+            <a href="#" class="fa fa-eye"></a>
+        </div>
+            <img src="p4.jpg" alt="">
+            <h3>Coconut plant</h3>
+            <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-alt"></i>
+            </div>
+            <div class="price">rs.200<span>rs.300</span></div>
+            <div class="quantity">
+                <span>quantity : </span>
+                <input type="number" min="1" max="1000" value="1">
+            </div>
+            <a href="#" class="btn">add to cart</a>
+        </div>
+
+        <div class="box">
+            <span class="discount ">50%</span>
+        <div class="icons">
+            <a href="#" class="fa fa-heart"></a>
+            <a href="#" class="fa fa-share"></a>
+            <a href="#" class="fa fa-eye"></a>
+        </div>
+            <img src="p5.jpg" alt="">
+            <h3>Dragon Fruit plant</h3>
+            <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-alt"></i>
+            </div>
+            <div class="price">rs.300<span>rs.600</span></div>
+            <div class="quantity">
+                <span>quantity : </span>
+                <input type="number" min="1" max="1000" value="1">
+            </div>
+            <a href="#" class="btn">add to cart</a>
+        </div>
+
+        <div class="box">
+            <span class="discount ">50%</span>
+        <div class="icons">
+            <a href="#" class="fa fa-heart"></a>
+            <a href="#" class="fa fa-share"></a>
+            <a href="#" class="fa fa-eye"></a>
+        </div>
+            <img src="p6.jpg" alt="">
+            <h3>Orange Plant</h3>
+            <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-alt"></i>
+            </div>
+            <div class="price">rs.200<span>rs.300</span></div>
+            <div class="quantity">
+                <span>quantity : </span>
+                <input type="number" min="1" max="1000" value="1">
+            </div>
+            <a href="#" class="btn">add to cart</a>
+        </div>
+
+        <div class="box">
+            <span class="discount ">50%</span>
+        <div class="icons">
+            <a href="#" class="fa fa-heart"></a>
+            <a href="#" class="fa fa-share"></a>
+            <a href="#" class="fa fa-eye"></a>
+        </div>
+            <img src="p7.jpg" alt="">
+            <h3>guava Plant</h3>
+            <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half"></i>
+            </div>
+            <div class="price">rs.200<span>rs.300</span></div>
+            <div class="quantity">
+                <span>quantity : </span>
+                <input type="number" min="1" max="1000" value="1">
+            </div>
+            <a href="#" class="btn">add to cart</a>
+        </div>
+
+    <section class="contact" id="contact">
+        <h1 class="heading"> <span>contact </span>us</h1>
+
+        <form action="">
+
+            <div class="inputBox">
+                <input type="text" placeholder="name">
+                <input type="email" placeholder="email">
+            </div>
+
+            <div class="inputBox">
+                <input type="number" placeholder="number">
+                <input type="text" placeholder="subject">
+            </div>
+
+            <textarea placeholder="message" name="" id="" cols="30" rows="10"></textarea>
+
+            <input type="submit" value="send message" class="btn">
+        </form>
+    </section>
+
+    <section class="newsletter">
+
+        <h3>sibscripe us for latest updates</h3>
+
+        <form action="">
+            <input class="box" type="email" placeholder="enter your email">
+            <input type="submit" value="subscribe" class="btn">
+        </form>
+    </section>
+
+    <section class="footer">
+        <div class="box-container">
+
+            <div class="box">
+                <a href="#" class="logo"><i class="fa fa-shopping-basket"></i>PLANT</a>
+                <p>My garden contains different types of plants. For instance,<br>
+                     it has different flowers such as roses, sunflowers, Lilies, daisies.<br>
+                      These flowers are the easiest to grow and flourish the environment with their beautiful smells.<br>
+                       Moreover, the colors of these flowers make a garden look beautiful.</p>
+                       
+                       <div class="share">
+                        <i class="btn fa fa-facebook-f"></i>
+                        <i class="btn fa fa-twitter"></i>
+                        <i class="btn fa fa-instagram"></i>
+                        <i class="btn fa fa-linkedin"></i>
+                       </div>
+            </div>
+             <div class="box">
+                <h3>our location</h3>
+                <div class="links">
+                    <a href="#">INDIA</a>
+                    <a href="#">USA</a>
+                    <a href="#">FRANCE</a>
+                    <a href="#">JAPAN</a>
+                    <a href="#">RUSSIA</a>
+                </div>
+             </div>
+
+             <div class="box">
+                <h3>quick links</h3>
+                <div class="links">
+                    <a href="#">home</a>
+                    <a href="#">product</a>
+                    <a href="#">contact</a>
+                </div>
+            </div>
+            <div class="box">
+               <h3>download app</h3>
+               <div class="links">
+                 <a href="#">google play</a>
+                 <a href="#">window xp</a>
+                 <a href="#">app store</a>
+               </div>
+            </div>
+        </div>    
+
+    </section>
+</body>
+</html>
